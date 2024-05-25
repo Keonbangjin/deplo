8c46aa937c3428ceaa203021ddf14c9f72e7d407
Keonbangjin <ochilovasliddin27@gmail.com>
2024-05-25 16:52:16 +0500

    added hooks and HOC

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// src/App.js
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ProtectedRoute from './components/ProtectedRoute';
 
-const Client = React.lazy(() => import("./pages/client"))
-const Login = React.lazy(() => import("./pages/login"))
-const Admin = React.lazy(() => import("./pages/admin"))
-
-
+const Client = React.lazy(() => import('./pages/client'));
+const Login = React.lazy(() => import('./pages/login'));
+const Admin = React.lazy(() => import('./pages/admin'));
 
 function App() {
   return (
@@ -13,12 +13,19 @@ function App() {
       <React.Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/" element={<Client />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="login" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute>
+                <Admin />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </React.Suspense>
     </BrowserRouter>
-  )
+  );
 }
-export default App;
 
+export default App;
